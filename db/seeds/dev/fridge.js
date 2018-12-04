eb200056846faeb34b1a25e1b6c199cc94039289
Tim Garrity <tag.garrity@gmail.com>
2018-12-03 20:30:04 -0700

    seed all tables iwth data

--- db/seeds/dev/fridge.js
+++ db/seeds/dev/fridge.js
@@ -1,12 +1,33 @@
 const mockRecipes = [
   {
     recipe_name: 'chicken pot pie',
-    ingredients: ['chicken', 'carrots', 'peas', 'pie crust', 'gravy'],
+    ingredients: [
+      {
+        ingredient_name: 'chicken',
+        aisle: 'meat',
+      }, 
+      {
+        ingredient_name:'carrots', 
+        aisle: 'produce'}, 
+      {
+        ingredient_name:'peas', 
+        aisle: 'produce'}, 
+      {
+        ingredient_name:'pie crust', 
+        aisle: 'produce'}, 
+      {
+        ingredient_name:'gravy', 
+        aisle: 'produce'}
+    ],
     steps: ['cook the chicken', 'make the rest of the pot pie'],
   },
   {
     recipe_name: 'pot roast',
-    ingredients: ['pot roast', 'nice taters', 'gravy', 'carrots'],
+    ingredients: [
+      {aisle: 'gravy',ingredient_name:'pot roast'}, 
+      {aisle: 'gravy',ingredient_name:'nice taters'}, 
+      {aisle: 'gravy',ingredient_name:'gravy'}, 
+      {aisle: 'gravy',ingredient_name:'carrots'}],
     steps: [
       'open crockpot',
       'insert potatoes',
@@ -24,27 +45,35 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       const recipePromises = mockRecipes.map(async recipe => {
         const recipeId = await createRecipe(knex, recipe);
+        await Promise.all(recipe.steps.map((step, index) => createSteps(knex, step, recipeId[0], index)))
         const ingredientIds = await Promise.all(
           recipe.ingredients.map(ingredient =>
             createIngredient(knex, ingredient),
           ),
         );
         const joinedIds = await Promise.all(
-          ingredientIds.map(id => joinIngredients(knex, id, recipeId)),
+          ingredientIds.map(id => joinIngredients(knex, id[0], recipeId[0])),
         );
       });
       return Promise.all(recipePromises);
-    });
+    })
+    .catch(error => console.log(error))
 };
 
 function createRecipe(knex, recipe) {
-  return knex('recipes').insert({recipe_name: recipe.name}, 'id');
+  return knex('recipes').insert({recipe_name: recipe.recipe_name}, 'id');
 }
 
 function createIngredient(knex, ingredient) {
-  return knex('ingredients').insert({ingredient_name: ingredient});
+  const { ingredient_name, aisle } = ingredient
+  return knex('ingredients').insert({ingredient_name, aisle}, 'id');
 }
 
 function joinIngredients(knex, ingredient_id, recipe_id) {
-  return knex('recipe_ingredients').insert({ingredient_id, recipe_id});
+  return knex('recipe_ingredients').insert({ingredient_id, recipe_id}, 'id');
+}
+
+function createSteps(knex, step_text, recipe_id, step_num) {
+  step_num += 1
+  return knex('recipe_steps').insert({step_num, step_text, recipe_id}, 'id')
 }
