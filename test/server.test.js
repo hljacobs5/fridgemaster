315e39ca134a8329eef94b1095af38c74e5f3275
Dina <dcaraballo26@gmail.com>
2018-12-04 14:48:14 -0700

    Refactor response body test to include nested object

--- test/server.test.js
+++ test/server.test.js
@@ -21,13 +21,25 @@ describe('server', () => {
         });
     });
 
-    it('should return an array', done => {
+    it('should return an array of objects', done => {
+      const testObject = { 
+        id: 9,
+        ingredient_name: 'carrots',
+        aisle: 'gravy',
+        created_at: '2018-12-04T21:18:14.999Z',
+        updated_at: '2018-12-04T21:18:14.999Z' 
+      }
       chai.request(app)
         .get('/api/v1/ingredients')
         .end((error, res) => {
           expect(res.body).to.be.a('array')
+          expect(res.body).to.deep.nested.include(testObject)
           done();
         });
     });
+
+    // it('should ', done => {
+
+    // });
   });
 });
