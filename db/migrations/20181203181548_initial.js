347a2c660001e35047cd4a160b103bd78a53a1c2
Tim Garrity <tag.garrity@gmail.com>
2018-12-03 18:33:29 -0700

    finish exports down for shema

--- db/migrations/20181203181548_initial.js
+++ db/migrations/20181203181548_initial.js
@@ -10,11 +10,35 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('recipes', (table) => {
       table.increments('id').primary()
       table.string('recipe_name')
+      table.timestamps(true, true)
+    }),
+    knex.schema.createTable('recipe_ingredients', (table) => {
+      table.increments('id').primary()
+      table.integer('recipe_id').unsigned()
+      table.foreign('recipe_id')
+        .references('recipes.id')
+      table.integer('ingredient_id').unsigned()
+      table.foreign('ingredient_id')
+        .references('ingredients.id')
+      table.timestamps(true, true)
+    }),
+    knex.schema.createTable('recipe_steps', (table) => {
+      table.increments('id').primary()
+      table.integer('step_num').unsigned()
+      table.string('step_text')
+      table.integer('recipe_id').unsigned()
+      table.foreign('recipe_id')
+        .references('recipes.id')
+      table.timestamp(true, true)
     })
-
   ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('recipe_steps'),
+    knex.schema.dropTable('recipe_ingredients'),
+    knex.schema.dropTable('recipes'),
+    knex.schema.dropTable('ingredients')
+  ])
 };
