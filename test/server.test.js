e7cc03746924020828bda41b8e09f890a4886ee1
Drake Bott <bott.da@gmail.com>
2018-12-04 15:26:47 -0700

    Test GET /api/v1/recipes

--- test/server.test.js
+++ test/server.test.js
@@ -10,36 +10,44 @@ describe('server', () => {
     beforeEach(done => {
       done();
     });
+    describe('GET', () => {
+      it('should return a 200 status', done => {
+        chai
+          .request(app)
+          .get('/api/v1/ingredients')
+          .end((error, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+      });
 
-    it('should return a 200 status', done => {
-      chai
-        .request(app)
-        .get('/api/v1/ingredients')
-        .end((error, res) => {
-          expect(res).to.have.status(200);
-          done();
-        });
+      it('should return an array of objects', done => {
+        chai
+          .request(app)
+          .get('/api/v1/ingredients')
+          .end((error, res) => {
+            expect(res.body).to.be.a('array');
+            expect(res.body[0]).to.have.property('id');
+            expect(res.body[0]).to.have.property('ingredient_name');
+            expect(res.body[0]).to.have.property('created_at');
+            expect(res.body[0]).to.have.property('updated_at');
+            done();
+          });
+      });
     });
-
-    it('should return an array of objects', done => {
-      const testObject = { 
-        id: 9,
-        ingredient_name: 'carrots',
-        aisle: 'gravy',
-        created_at: '2018-12-04T21:18:14.999Z',
-        updated_at: '2018-12-04T21:18:14.999Z' 
-      }
-      chai.request(app)
-        .get('/api/v1/ingredients')
-        .end((error, res) => {
-          expect(res.body).to.be.a('array')
-          expect(res.body).to.deep.nested.include(testObject)
-          done();
-        });
+  });
+  describe('/api/v1/recipes', () => {
+    describe('GET', () => {
+      it('should return a 200 status', done => {
+        chai
+          .request(app)
+          .get('/api/v1/recipes')
+          .end((error, res) => {
+            expect(error).to.equal(null);
+            done();
+          });
+      });
     });
-
-    // it('should ', done => {
-
-    // });
   });
 });
+// it('should ', done => {});
