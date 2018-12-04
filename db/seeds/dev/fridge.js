e531b384f8b2deda193281bd912b750737580e55
Drake Bott <bott.da@gmail.com>
2018-12-03 19:42:42 -0700

    Create seeding methods

--- db/seeds/dev/fridge.js
+++ db/seeds/dev/fridge.js
@@ -1,43 +1,50 @@
 const mockRecipes = [
   {
     recipe_name: 'chicken pot pie',
-    ingredients: [
-      'chicken',
-      'carrots',
-      'peas',
-      'pie crust',
-      'gravy'
-    ],
-    steps: [
-      'cook the chicken',
-      'make the rest of the pot pie'
-    ]
+    ingredients: ['chicken', 'carrots', 'peas', 'pie crust', 'gravy'],
+    steps: ['cook the chicken', 'make the rest of the pot pie'],
   },
   {
     recipe_name: 'pot roast',
-    ingredients: [
-      'pot roast',
-      'nice taters',
-      'gravy',
-      'carrots'
-    ],
+    ingredients: ['pot roast', 'nice taters', 'gravy', 'carrots'],
     steps: [
       'open crockpot',
       'insert potatoes',
       'insert pot roast',
-      'yay! you did it'
-    ]
-  }
-]
+      'yay! you did it',
+    ],
+  },
+];
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+  return knex('recipe_steps')
+    .del()
+    .then(() => knex('recipe_ingredients').del())
+    .then(() => knex('recipes').del())
+    .then(() => knex('ingredients').del())
+    .then(() => {
+      const recipePromises = mockRecipes.map(async recipe => {
+        const recipeId = await createRecipe(knex, recipe);
+        const ingredientIds = await Promise.all(
+          recipe.ingredients.map(ingredient =>
+            createIngredient(knex, ingredient),
+          ),
+        );
+        const joinedIds = await Promise.all(
+          ingredientIds.map(id => joinIngredients(knex, id, recipeId)),
+        );
+      });
+      return Promise.all(recipePromises);
     });
 };
+
+function createRecipe(knex, recipe) {
+  return knex('recipes').insert({recipe_name: recipe.name}, 'id');
+}
+
+function createIngredients(knex, ingredient) {
+  return knex('ingredients').insert({ingredient_name: ingredient});
+}
+
+function joinIngredients(knex, ingredient_id, recipe_id) {
+  return knex('recipe_ingredients').insert({ingredient_id, recipe_id});
+}
